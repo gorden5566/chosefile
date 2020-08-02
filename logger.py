cd17a742499c34885c65825fc7e012ec6a5d3747
gorden5566 <gorden5566@126.com>
2020-08-03 01:06:09 +0800

    refactor

--- logger.py
+++ logger.py
@@ -6,21 +6,22 @@ import time
 
 class Logger:
     def __init__(self, consolecontent):
-        self.consolecontent = consolecontent
-        self.createfilelogger()
+        self.console_content = consolecontent
+        self.file_logger = self.create_file_logger()
 
-    def createfilelogger(self):
+    @staticmethod
+    def create_file_logger():
         today = time.strftime("%Y%m%d", time.localtime())
         f = open(today + ".log", "a+")
-        self.filelogger = f
+        return f
 
     def close(self):
-        self.filelogger.close()
+        self.file_logger.close()
 
     def Log(self, message):
         now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        formatmessage = now + "\t" + message + "\n"
+        format_message = now + "\t" + message + "\n"
 
-        self.consolecontent.AppendText(formatmessage)
+        self.console_content.AppendText(format_message)
 
-        self.filelogger.write(formatmessage)
+        self.file_logger.write(format_message)
