2c061b55da60363fabecf25a92dcb0ee7bf6f97c
gorden5566 <gorden5566@126.com>
2020-08-04 01:28:17 +0800

    setting

--- setting.py
+++ setting.py
@@ -55,15 +55,14 @@ class Setting:
         current_path = os.getcwd()
         config_path = os.path.join(current_path, config_name)
         if not os.path.exists(config_path):
-            self.logger.Log("[配置文件不存在]\t" + config_path)
-            return None
+            self.init_config(config_path)
 
         conf = configparser.ConfigParser()
         conf.read(config_path, encoding="utf-8")
 
         section = "default"
-        config = {'sourceDir': self.get_config_val(conf, section, "sourceDir", "."),
-                  'targetDir': self.get_config_val(conf, section, "targetDir", "."),
+        config = {'sourceDir': self.get_config_val(conf, section, "sourceDir", ""),
+                  'targetDir': self.get_config_val(conf, section, "targetDir", ""),
                   'extName': self.get_config_val(conf, section, "extName", ".dwg"),
                   'columnTitle': self.get_config_val(conf, section, "columnTitle", "图号"),
                   'excelPath': self.get_config_val(conf, section, "excelPath", ""),
@@ -71,6 +70,26 @@ class Setting:
                   }
 
         return config
+
+    def init_config(self, config_path):
+        content_arr = [
+            "[default]\n",
+            "; 模板文件扩展名\n",
+            "extName = .dwg\n\n",
+            "; 模板文件路径\n",
+            "sourceDir = \n\n",
+            "; 默认的目标文件夹\n",
+            "targetDir = \n\n",
+            "; excel文件路径\n",
+            "excelPath = \n\n",
+            "; 标题，只处理该标题下的单元格\n",
+            "columnTitle = 图号\n\n",
+            "; 索引最大深度\n",
+            "maxDepth = 5\n"
+        ]
+        content = "".join(content_arr)
+        with open('config.ini', 'w') as configfile:
+            configfile.write(content)
 
     def get_config_val(self, conf, section, key, default):
         try:
